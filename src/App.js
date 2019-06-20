7b10810b6c5f068d218d9bf6056d1f37c1ffd958
Michael Friedman <contact@michaeldavidfriedman.com>
2019-06-20 11:48:41 -0700

    Successfully logging in
    Logging into app and login state persisting through refresh

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Nav from './Nav'
 import Channel from './Channel'
+import { firebase } from './firebase'
 
 export default function App() {
-  return (
+  const [user, setUser] = useState(null)
+
+  useEffect(() =>
+    firebase
+      .auth()
+      .onAuthStateChanged(user => (user ? setUser(user) : setUser(null)))
+  )
+
+  const handleSignIn = async () => {
+    const provider = new firebase.auth.GoogleAuthProvider()
+    await firebase.auth().signInWithPopup(provider)
+  }
+  return user ? (
     <div className="App">
       <Nav />
       <Channel />
+    </div>
+  ) : (
+    <div className="Login">
+      <h1>Chat!</h1>
+      <button onClick={handleSignIn}>Sign in with Google</button>
     </div>
   )
 }
