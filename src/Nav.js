54a2c47f0af33a3aedff46a55dac8eaf3af70670
Michael Friedman <contact@michaeldavidfriedman.com>
2019-06-20 12:23:53 -0700

    Update Nav.js
    Implement sign out functionality

--- src/Nav.js
+++ src/Nav.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useCollection } from './useCollection'
+import { firebase } from './firebase'
 
 export default function Nav({ user }) {
   const channels = useCollection('channels')
@@ -15,7 +16,14 @@ export default function Nav({ user }) {
         <div>
           <div>{user.displayName}</div>
           <div>
-            <button className="text-button">log out</button>
+            <button
+              onClick={() => {
+                firebase.auth().signOut()
+              }}
+              className="text-button"
+            >
+              log out
+            </button>
           </div>
         </div>
       </div>
