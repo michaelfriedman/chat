ed73bcfa1f9abb8753db87a6af11d3a3539caf65
Michael Friedman <contact@michaeldavidfriedman.com>
2019-06-20 10:25:33 -0700

    Render messages
    Dynamically render chat messages from real-time database

--- src/Messages.js
+++ src/Messages.js
@@ -1,31 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { db } from './firebase'
 
 function Messages() {
+  const [messages, setMessages] = useState([])
+  useEffect(() => {
+    db.collection('channels')
+      .doc('general')
+      .collection('messages')
+      .onSnapshot(snapshot => {
+        const docs = []
+        snapshot.forEach(doc => {
+          docs.push({
+            ...doc.data(),
+            id: doc.id
+          })
+        })
+        setMessages(docs)
+      })
+  }, [])
   return (
     <div className="Messages">
       <div className="EndOfMessages">That's every message!</div>
-      <div>
-        <div className="Day">
-          <div className="DayLine" />
-          <div className="DayText">12/6/2018</div>
-          <div className="DayLine" />
-        </div>
-        <div className="Message with-avatar">
-          <div className="Avatar" />
-          <div className="Author">
-            <div>
-              <span className="UserName">Michael Friedman </span>
-              <span className="TimeStamp">3:37 PM</span>
+      {messages.map((message, index) => {
+        return index === 0 ? (
+          <div key={index}>
+            <div className="Day">
+              <div className="DayLine" />
+              <div className="DayText">12/6/2018</div>
+              <div className="DayLine" />
             </div>
-            <div className="MessageContent">Alright, lets do this.</div>
+            <div className="Message with-avatar">
+              <div className="Avatar" />
+              <div className="Author">
+                <div>
+                  <span className="UserName">Michael Friedman </span>
+                  <span className="TimeStamp">3:37 PM</span>
+                </div>
+                <div className="MessageContent">{message.text}</div>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      <div>
-        <div className="Message no-avatar">
-          <div className="MessageContent">works now?</div>
-        </div>
-      </div>
+        ) : (
+          <div key={index}>
+            <div className="Message no-avatar">
+              <div className="MessageContent">{message.text}</div>
+            </div>
+          </div>
+        )
+      })}
     </div>
   )
 }
