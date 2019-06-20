d9f8726d46ecc22982d612165a7a7b3b9ccf9ac8
Michael Friedman <contact@michaeldavidfriedman.com>
2019-06-19 17:07:18 -0700

    Setup chat app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Nav">
+        <div className="User">
+          <img
+            className="UserImage"
+            alt="whatever"
+            src="https://placekitten.com/64/64"
+          />
+          <div>
+            <div>Ryan Peterson Florence</div>
+            <div>
+              <button className="text-button">log out</button>
+            </div>
+          </div>
+        </div>
+        <nav className="ChannelNav">
+          <a href="/channel/awesome"># awesome</a>
+          <a className="active" href="/channel/general">
+            # general
+          </a>
+        </nav>
+      </div>
+      <div className="Channel">
+        <div className="ChannelMain">
+          <div className="ChannelInfo">
+            <div className="Topic">
+              Topic: <input className="TopicInput" value="Awesome stuff" />
+            </div>
+            <div className="ChannelName">#general</div>
+          </div>
+          <div className="Messages">
+            <div className="EndOfMessages">That's every message!</div>
+            <div>
+              <div className="Day">
+                <div className="DayLine" />
+                <div className="DayText">12/6/2018</div>
+                <div className="DayLine" />
+              </div>
+              <div className="Message with-avatar">
+                <div className="Avatar" />
+                <div className="Author">
+                  <div>
+                    <span className="UserName">Ryan Florence </span>
+                    <span className="TimeStamp">3:37 PM</span>
+                  </div>
+                  <div className="MessageContent">Alright, lets do this.</div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="Message no-avatar">
+                <div className="MessageContent">works now?</div>
+              </div>
+            </div>
+          </div>
+          <div className="ChatInputBox">
+            <input className="ChatInput" placeholder="Message #general" />
+          </div>
+        </div>
+        <div className="Members">
+          <div>
+            <div className="Member">
+              <div className="MemberStatus offline" />
+              Ryan Florence
+            </div>
+            <div className="Member">
+              <div className="MemberStatus online" />
+              cleverbot
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
