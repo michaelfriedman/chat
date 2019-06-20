ae730450d5b57791800ede2aa3410dde38aa89f9
Michael Friedman <contact@michaeldavidfriedman.com>
2019-06-20 00:25:34 -0700

    rendering dynamicall generated channels list

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   const [channels, setChannels] = useState([])
 
   useEffect(() => {
-    db.collection('channels').onSnapshot(snapshot => {
+    return db.collection('channels').onSnapshot(snapshot => {
       const docs = []
       snapshot.forEach(doc => {
         docs.push({
@@ -27,6 +27,7 @@ function App() {
           id: doc.id
         })
       })
+      setChannels(docs)
     })
   }, [])
   return (
