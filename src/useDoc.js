8da273e41df9a85be0a3f7e279df12bf404801ef
Michael Friedman <contact@michaeldavidfriedman.com>
2019-06-20 22:14:17 -0700

    Prevent Flickers
    Prevent flickers using a simple JavaScript cache

--- src/useDoc.js
+++ src/useDoc.js
@@ -1,24 +1,33 @@
 import { useState, useEffect } from 'react'
 import { db } from './firebase'
 
+const cache = {}
+const pendingCache = {}
+
 export function useDoc(path) {
-  const [doc, setDoc] = useState()
+  const [doc, setDoc] = useState(cache[path])
   useEffect(() => {
+    if (doc) {
+      return
+    }
     let stillMounted = true
 
-    db.doc(path)
-      .get()
-      .then(doc => {
-        if (stillMounted) {
-          setDoc({
-            ...doc.data(),
-            id: doc.id
-          })
+    const pending = pendingCache[path]
+    const promise = pending || (pendingCache[path] = db.doc(path).get())
+
+    promise.then(doc => {
+      if (stillMounted) {
+        const user = {
+          ...doc.data(),
+          id: doc.id
         }
-      })
+        setDoc(user)
+        cache[path] = user
+      }
+    })
     return () => {
       stillMounted = false
     }
-  }, [path])
+  }, [path, doc])
   return doc
 }
