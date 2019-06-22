c581e2aed17d907ed4c36bc4854fa8cc5e77a137
Michael Friedman <contact@michaeldavidfriedman.com>
2019-06-21 19:05:36 -0700

    update deep document values

--- src/Channel.js
+++ src/Channel.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Members from './Members'
 import ChannelInfo from './ChannelInfo'
 import Messages from './Messages'
 import ChatInputBox from './ChatInputBox'
+import { db } from './firebase'
 
 export default function Channel({ user, channelId }) {
+  useEffect(() => {
+    db.doc(`users/${user.uid}`).update({
+      [`channels.${channelId}`]: true
+    })
+  }, [user.uid, channelId])
+
   return (
     <div className="Channel">
       <div className="ChannelMain">
