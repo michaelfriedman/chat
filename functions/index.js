d5f31836c56676ca75149903c1953e9fabc63c05
Michael Friedman <contact@michaeldavidfriedman.com>
2019-06-22 21:28:04 -0700

    organize cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,6 @@
-const functions = require('firebase-functions')
+const admin = require('firebase-admin')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+admin.initializeApp()
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send('Hello from Firebase!')
-  const randomNumber = Math.random() * 10
-    randomNumber < 5 ? console.error('Error, it was less than 5.') : console.warn('Just warning you, it was greater than 5.')
-})
+exports.onUserStatusChanged = require('./triggers/onUserStatusChanged')
+exports.helloWorld = require('./routes/helloWorld')
